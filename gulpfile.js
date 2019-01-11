87404ede4a77b9c0cd401cc1aa17d7d8ca0969f7
Pavel <p.v.hromov@gmail.com>
2019-01-11 15:01:01 -0500

    Completed header and Grand Canyon

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,8 @@
-var gulp = require('gulp'),
-watch = require('gulp-watch'),
-postcss = require('gulp-postcss'),
-autoprefixer = require('autoprefixer'),
-cssvars = require('postcss-simple-vars'),
-nested = require('postcss-nested'),
-cssImport = require('postcss-import'),
-browserSync= require('browser-sync').create();
-
-gulp.task('default', function() {
-	console.log("Hoorey, you created gulp task.");
-});
-
-gulp.task('html', function () {
-	console.log("Imagine something...");
-});
-
-gulp.task('styles', function () {
-	return gulp.src('./assets/styles/styles.css')
-	.pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
-	.pipe(gulp.dest('./temp/styles'));
-});
-
-gulp.task('watch', function() {
-	browserSync.init({
-		notify: false,
-		server: {
-			baseDir: ""
-		}
-
-	});
-
-	watch('./index.html', function() {
-		browserSync.reload();
-	});
-
-	watch('./assets/styles/**/*.css', function() {
-		gulp.start('cssInject');
-	});
-});
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
 
 
-gulp.task('cssInject', ['styles'], function() {
-	return gulp.src('./temp/styles/styles.css')
-	 .pipe(browserSync.stream());
 
-});
+
+
+
