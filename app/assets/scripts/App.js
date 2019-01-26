97e1eff9b1bbeefc343963689cd25c0bf0119c0b
Pavel <p.v.hromov@gmail.com>
2019-01-26 15:14:17 -0500

    Photo gallery

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,12 @@
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
+import $ from 'jquery';
+import StickyHeader from './modules/StickyHeader';
 
 var mobileMenu = new MobileMenu();
+var revealOnScroll = new RevealOnScroll();
+var stickyHeader = new StickyHeader();
+
 
 
 
